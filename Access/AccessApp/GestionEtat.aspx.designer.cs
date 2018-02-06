0147ae2f4797b81895cf403e1f85f69e78eece30
Lepape Yorick <yorick-1996@hotmail.com>
2018-02-06 08:08:17 +0100

    ok

--- Access/AccessApp/GestionEtat.aspx.designer.cs
+++ Access/AccessApp/GestionEtat.aspx.designer.cs
@@ -10,15 +10,15 @@
 namespace AccessApp {
     
     
-    public partial class SiteMaster {
+    public partial class GestionEtat {
         
         /// <summary>
-        /// Contrôle MainContent.
+        /// Contrôle form1.
         /// </summary>
         /// <remarks>
         /// Champ généré automatiquement.
         /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
     }
 }
