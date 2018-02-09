2c53e2c8afa49c829dd209eaa8260e183d4d6512
Sylvain Fissiaux <fissiaux.sylvain@gmail.com>
2018-02-09 11:15:15 +0100

    Requêtes

--- Access/AccessApp/Consts.cs
+++ Access/AccessApp/Consts.cs
@@ -9,6 +9,8 @@ namespace AccessApp
     {
         // DATABASE TABLE OR VIEW
         public static readonly string ACCESS_REQUEST_TABLE = "EPIDESK.ACCESS_REQUEST";
+        public static readonly string CONTACTS_TABLE = "EPIDESK.CONTACTS";
+        public static readonly string TICKETS_TABLE = "EPIDESK.TICKETS";
 
         public static readonly string ACCESS_REQUEST_VIEW = "ACCESS_REQUESTS_VIEW";
         public static readonly string ACCESS_REQUEST_STATUS = "ACCESS_REQUESTS_STATUS_VIEW";
