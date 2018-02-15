d3a34ddc4368504ceb4fa1c91062e5984e2a14be
Lepape Yorick <yorick-1996@hotmail.com>
2018-02-15 11:56:56 +0100

    ok

--- Access/AccessApp/GestionMateriel.aspx.cs
+++ Access/AccessApp/GestionMateriel.aspx.cs
@@ -27,6 +27,12 @@ namespace AccessApp
         {
             DataSet ds = DAL.getProductPerEpiId(id);
             L_EpiID.Text = String.Format("{0}",ds.Tables[0].Rows[0]["EPIID"]);
+            L_Marque.Text = (string) ds.Tables[0].Rows[0]["NAME"] + " " + (string) ds.Tables[0].Rows[0]["MODELE"];
+            L_NumSe.Text = (string) ds.Tables[0].Rows[0]["SERIAL_NUMBER"];
+            L_Statut.Text = (string)ds.Tables[0].Rows[0]["STOCK_STATUS"];
+            L_Loc.Text = (string)ds.Tables[0].Rows[0]["LOCALISATION_ID"];
+            L_Agent.Text = (string)ds.Tables[0].Rows[0]["LAST_NAME"] + " " + (string)ds.Tables[0].Rows[0]["FIRST_NAME"];
+            L_Date.Text = String.Format("{0}", ds.Tables[0].Rows[0]["OPERATION_DATE"]);
         }
     }
 }
