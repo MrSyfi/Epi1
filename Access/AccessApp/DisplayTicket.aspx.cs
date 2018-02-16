eb03a714be03c0405583173d3c1cda95f74d2fae
Lepape Yorick <yorick-1996@hotmail.com>
2018-02-16 15:01:27 +0100

    ok

--- Access/AccessApp/DisplayTicket.aspx.cs
+++ Access/AccessApp/DisplayTicket.aspx.cs
@@ -65,7 +65,8 @@ namespace AccessApp
 
             foreach (DataRow row in dt.Rows)
             {
-                L_Body.Text +=  "< div class='jumbotron'>"+ row["LOG"].ToString() + "</div>";
+                L_Body.Text += "< div class='jumbotron'>" + row["LOG"].ToString() + "</div>";
+            }
         }
     }
 }
