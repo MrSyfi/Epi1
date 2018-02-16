9169026d088b8bf29e3fc5fde251c8b7ae3d02c6
Sylvain Fissiaux <fissiaux.sylvain@gmail.com>
2018-02-16 14:15:30 +0100

    IconObsolete

--- Access/AccessApp/GestionMateriel.aspx.cs
+++ Access/AccessApp/GestionMateriel.aspx.cs
@@ -68,7 +68,7 @@ namespace AccessApp
                     case "TRANSIT": L_Histo.Text += "<div class='cd-timeline-img cd-transit'><img src='outstock.png' alt='transit'></div>"; break;
                     case "INSTALLED": L_Histo.Text += "<div class='cd-timeline-img cd-installed'><img src='installed.png' alt='installed'></div>"; break;
                     case "UNDER_REPAIR": L_Histo.Text += "<div class='cd-timeline-img cd-repairing'><img src='repair.png' alt='repairing'></div>"; break;
-                    case "OBSOLETE": L_Histo.Text += "<div class='cd-timeline-img cd-obsolete'><img src='expired.png' alt='obsolete'></div>"; break;
+                    case "OBSOLETE": L_Histo.Text += "<div class='cd-timeline-img cd-obsolete'><img src='deleteobsolete.png' alt='obsolete'></div>"; break;
                 }
                 
 
