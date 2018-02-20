b3648fedbc121eb0cf2706ecaf1b3a83f749391f
Sylvain Fissiaux <fissiaux.sylvain@gmail.com>
2018-02-20 12:13:40 +0100

    Ok for apply ?

--- Access/AccessApp/EpiStock.aspx.cs
+++ Access/AccessApp/EpiStock.aspx.cs
@@ -20,7 +20,13 @@ namespace AccessApp
 
         protected void B_apply_Click(object sender, EventArgs e)
         {
-
+            // Get the localisation id;
+            //string locId = DAL.SelectLocalisationId(TB_id_local.Text).Tables[0].Rows[0]["ID"].ToString();
+            //if (locId == string.Empty)
+                // Unknown Localisation..
+               // DAL.InsertLocalisationId(TB_id_local.Text);
+            //locId = DAL.SelectLocalisationId(TB_id_local.Text).Tables[0].Rows[0]["ID"].ToString();
+            //DAL.InsertInHistoric(TB_id_resp.Text, DDL_status.SelectedValue.ToString(), TB_id_materiel.Text, locId);
         }
 
         protected void TB_id_materiel_TextChanged(object sender, EventArgs e)
