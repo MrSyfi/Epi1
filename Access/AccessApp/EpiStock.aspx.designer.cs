9f18e4c5d4d421c8e8ea6ab23866f6769a9ac918
Sylvain Fissiaux <fissiaux.sylvain@gmail.com>
2018-02-21 10:34:08 +0100

    Am tabl

--- Access/AccessApp/EpiStock.aspx.designer.cs
+++ Access/AccessApp/EpiStock.aspx.designer.cs
@@ -58,15 +58,6 @@ namespace AccessApp {
         protected global::System.Web.UI.WebControls.TextBox TB_note;
         
         /// <summary>
-        /// Contrôle B_apply.
-        /// </summary>
-        /// <remarks>
-        /// Champ généré automatiquement.
-        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button B_apply;
-        
-        /// <summary>
         /// Contrôle L_obsolete.
         /// </summary>
         /// <remarks>
@@ -83,5 +74,14 @@ namespace AccessApp {
         /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button B_obsolete;
+        
+        /// <summary>
+        /// Contrôle B_apply.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button B_apply;
     }
 }
