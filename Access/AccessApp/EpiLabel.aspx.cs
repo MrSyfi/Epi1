193a7d80e0f16322197281ca4dcf7b0f7f1e0ccd
Sylvain Fissiaux <fissiaux.sylvain@gmail.com>
2018-02-22 10:11:24 +0100

    ZPL

--- Access/AccessApp/EpiLabel.aspx.cs
+++ Access/AccessApp/EpiLabel.aspx.cs
@@ -17,6 +17,19 @@ namespace AccessApp
         protected void B_afficher_Click(object sender, EventArgs e)
         {
 
+            PopulateZPL();
+        }
+
+        private void PopulateZPL()
+        {
+            // Code QR en ZPL : ^XA^FO100,100^BQN,2,10^FDYourTextHere^FS^XZ
+            L_result.Text = "";
+            L_result.Text = "^XA";
+            L_result.Text += "^FO300,100^BQN,2,10^"+TB_code.Text+"^FS";
+            L_result.Text += "CFA,30";
+            L_result.Text += "^FO350,500^FD" + TB_info.Text + "^FS";
+
+            L_result.Text += "^XZ";
         }
     }
 }
