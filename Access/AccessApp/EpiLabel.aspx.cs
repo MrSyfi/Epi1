e85c59d64e2f5b536ea9aaecfe8686020ba80fb3
Lepape Yorick <yorick-1996@hotmail.com>
2018-02-22 10:26:12 +0100

    ok

--- Access/AccessApp/EpiLabel.aspx.cs
+++ Access/AccessApp/EpiLabel.aspx.cs
@@ -25,9 +25,10 @@ namespace AccessApp
             // Code QR en ZPL : ^XA^FO100,100^BQN,2,10^FDYourTextHere^FS^XZ
             L_result.Text = "";
             L_result.Text = "^XA";
-            L_result.Text += "^FO300,100^BQN,2,10^"+TB_code.Text+"^FS";
+            L_result.Text += "^FO50,100^BXN,10,200^FD" + TB_code.Text+"^FS";
             L_result.Text += "CFA,30";
             L_result.Text += "^FO350,500^FD" + TB_info.Text + "^FS";
+
 
             L_result.Text += "^XZ";
         }
