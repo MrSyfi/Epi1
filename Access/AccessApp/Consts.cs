8a7b22c56ea3eda1d54d7fb01e6f6746a8f5938e
Lepape Yorick <yorick-1996@hotmail.com>
2018-02-26 12:09:48 +0100

    ok

--- Access/AccessApp/Consts.cs
+++ Access/AccessApp/Consts.cs
@@ -36,6 +36,10 @@ namespace AccessApp
         public static readonly string EMPTY_STRING = " - ";
         public static readonly int LABEL_STRING_LENGHT_LIMIT = 30;
 
+        public static readonly string CONST_NETWORK_EDUC_SERVER  = "VMHUINFORV02.epicura.lan";
+        public static readonly int CONST_NETWORK_EDUC_PORT = 2607;
         
+
+
     }
 }
---
+++ Access/AccessApp/RemoteDBConnection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Web;
+
+namespace AccessApp
+{
+    public class RemoteDBConnection
+    {
+        public Database database;
+        public bool Connect()
+        {
+            ChannelServices.RegisterChannel(new TcpClientChannel(), false);
+            database = (Database)Activator.GetObject(typeof(Database), string.Format("tcp://{0}:{1}/{2}",Consts.CONST_NETWORK_EDUC_SERVER, Consts.CONST_NETWORK_EDUC_PORT.ToString(), Consts.CONST_NETWORK_EDUC_SERVICE_NAME));
+            
+            return true;
+        }
+    }
+}
