f40e482a00b683eeaba4586cf7b383aaac34622c
Sylvain Fissiaux <fissiaux.sylvain@gmail.com>
2018-03-05 11:36:09 +0100

    if obsolete

--- Access/AccessApp/EpiStock.aspx.cs
+++ Access/AccessApp/EpiStock.aspx.cs
@@ -239,31 +239,40 @@ namespace AccessApp
 
         protected void B_obsolete_Click(object sender, EventArgs e)
         {
-
-            // Confirmation.
-            if (System.Windows.Forms.MessageBox.Show("Voulez-vous vraiment déclasser cet objet ?", "Déclassement",MessageBoxButtons.YesNo) == DialogResult.Yes)
+            if (TB_id_resp.Text != string.Empty)
             {
+                // Confirmation.
+                if (System.Windows.Forms.MessageBox.Show("Voulez-vous vraiment déclasser cet objet ?", "Déclassement", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
 
-                // Envoi Mail au responsable
-                DataSet ds = DAL.GetProductPerEpiId(CheckEpi());
-                string model = ds.Tables[0].Rows[0]["MODELE"].ToString();
-                string marque = ds.Tables[0].Rows[0]["NAME"].ToString();
-                string numSerie = ds.Tables[0].Rows[0]["SERIAL_NUMBER"].ToString();
-                ds = DAL.SelectContact(TB_id_resp.Text);
-                string nameAgent = ds.Tables[0].Rows[0]["LAST_NAME"].ToString() + " " + ds.Tables[0].Rows[0]["FIRST_NAME"].ToString();
-                string mailAgent = ds.Tables[0].Rows[0]["EMAIL"].ToString();
-                ds = DAL.GetRespMail();
-                string respMail = ds.Tables[0].Rows[0]["VALUE"].ToString();
+                    // Envoi Mail au responsable
+                    DataSet ds = DAL.GetProductPerEpiId(CheckEpi());
+                    string model = ds.Tables[0].Rows[0]["MODELE"].ToString();
+                    string marque = ds.Tables[0].Rows[0]["NAME"].ToString();
+                    string numSerie = ds.Tables[0].Rows[0]["SERIAL_NUMBER"].ToString();
+                    ds = DAL.SelectContact(TB_id_resp.Text);
+                    string nameAgent = ds.Tables[0].Rows[0]["LAST_NAME"].ToString() + " " + ds.Tables[0].Rows[0]["FIRST_NAME"].ToString();
+                    string mailAgent = ds.Tables[0].Rows[0]["EMAIL"].ToString();
+                    ds = DAL.GetRespMail();
+                    string respMail = ds.Tables[0].Rows[0]["VALUE"].ToString();
 
-                //MailSender.SendObsoleteEmail("resp", mailAgent, TB_id_materiel.Text, marque, model, numSerie, nameAgent);
-                MailSender.SendObsoleteEmail(respMail, mailAgent, CheckEpi(), marque, model, numSerie, nameAgent);
+                    //MailSender.SendObsoleteEmail("resp", mailAgent, TB_id_materiel.Text, marque, model, numSerie, nameAgent);
+                    MailSender.SendObsoleteEmail(respMail, mailAgent, CheckEpi(), marque, model, numSerie, nameAgent);
 
-                // Modif DB..
-                DAL.InsertInHistoric(TB_id_resp.Text, DDL_status.SelectedValue.ToString(), CheckEpi(), "0");
-                DAL.UpdateStockStatus(CheckEpi(), DDL_status.SelectedValue.ToString());
+                    // Modif DB..
+                    DAL.InsertInHistoric(TB_id_resp.Text, DDL_status.SelectedValue.ToString(), CheckEpi(), "0");
+                    DAL.UpdateStockStatus(CheckEpi(), DDL_status.SelectedValue.ToString());
 
-                Reset();
-                SetFocus();
+                    Reset();
+                    SetFocus();
+                }
+                else
+                {
+                    System.Web.HttpContext.Current.Response.Write("<SCRIPT LANGUAGE='JavaScript'>alert('Déclassement annulé !')</SCRIPT>");
+                }
+            } else
+            {
+                System.Web.HttpContext.Current.Response.Write("<SCRIPT LANGUAGE='JavaScript'>alert('Champs vides !')</SCRIPT>");
             }
 
 
