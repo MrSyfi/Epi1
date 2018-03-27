5e24f902303be9cedec3d5b8a14b206253309885
Sylvain Fissiaux <fissiaux.sylvain@gmail.com>
2018-03-27 13:37:57 +0200

    Print method

--- Access/AccessApp/CopyLabel.aspx.cs
+++ Access/AccessApp/CopyLabel.aspx.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Sockets;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -11,6 +13,30 @@ namespace AccessApp
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+
+        }
+
+        private void Print(string zpl)
+        {
+            using (TcpClient client = new TcpClient())
+            {
+                try
+                {
+                    client.Connect(DAL.SelectPrinterIP(DDL_Printer.SelectedValue).Tables[0].Rows[0]["VALUE"].ToString(), Consts.ZPL_PRINTERS_DEFAULT_PORT);
+
+                    using (StreamWriter writer = new StreamWriter(client.GetStream()))
+                    {
+
+                        writer.Write(zpl);
+                        writer.Flush();
+                    }
+                }
+                catch
+                {
+                    System.Web.HttpContext.Current.Response.Write("<SCRIPT LANGUAGE='JavaScript'>alert('Il y a eu une erreur lors de l'impression.')</SCRIPT>");
+                    // Possible exceptions ?!?
+                }
+            }
 
         }
     }
