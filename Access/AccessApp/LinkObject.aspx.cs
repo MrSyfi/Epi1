e016a5e04b0e45061df7384a0a9cc7b5409cd222
Sylvain Fissiaux <fissiaux.sylvain@gmail.com>
2018-03-28 12:53:19 +0200

    Squelette

--- Access/AccessApp/LinkObject.aspx.cs
+++ Access/AccessApp/LinkObject.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -17,6 +18,59 @@ namespace AccessApp
         protected void B_apply_Click(object sender, EventArgs e)
         {
 
+        }
+
+        protected void TB_id_ticket_TextChanged(object sender, EventArgs e)
+        {
+           DataSet _ds = DAL.SelectAllFromTicketId(TB_id_ticket.Text);
+            if (_ds.Tables[0].Rows.Count != 0)
+            {
+                SetFocus();
+
+            } else
+            {
+                TB_id_ticket.Text = string.Empty;
+                System.Web.HttpContext.Current.Response.Write("<SCRIPT LANGUAGE='JavaScript'>alert('Identifiant du ticket incorrect !')</SCRIPT>");
+                SetFocus();
+            }
+        }
+
+        protected void TB_id_op_TextChanged(object sender, EventArgs e)
+        {
+            DataSet _ds = DAL.SelectContact(TB_id_op.Text);
+            if (_ds.Tables[0].Rows.Count != 0)
+            {
+                SetFocus();
+            } else
+            {
+                TB_id_op.Text = string.Empty;
+                System.Web.HttpContext.Current.Response.Write("<SCRIPT LANGUAGE='JavaScript'>alert('Identifiant de l'opérateur incorrect !')</SCRIPT>");
+                SetFocus();
+            }
+        }
+
+        protected void TB_EpiID_TextChanged(object sender, EventArgs e)
+        {
+            DataSet _ds = DAL.GetProduct(TB_EpiID.Text);
+            if (_ds.Tables[0].Rows.Count != 0)
+            {
+                SetFocus();
+            } else
+            {
+                TB_EpiID.Text = string.Empty;
+                System.Web.HttpContext.Current.Response.Write("<SCRIPT LANGUAGE='JavaScript'>alert('Epi ID incorrect !')</SCRIPT>");
+                SetFocus();
+            }
+        }
+
+        public void SetFocus()
+        {
+            if (TB_id_ticket.Text == string.Empty)
+                TB_id_ticket.Focus();
+            else if (TB_id_op.Text == string.Empty)
+                TB_id_op.Focus();
+            else if (TB_EpiID.Text == string.Empty)
+                TB_EpiID.Focus();
         }
     }
 }
