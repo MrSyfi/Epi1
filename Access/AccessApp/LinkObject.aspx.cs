2de865926f841e9a9ac774e15a720b3d6bab5c6e
Lepape Yorick <yorick-1996@hotmail.com>
2018-03-28 13:13:46 +0200

    modif click

--- Access/AccessApp/LinkObject.aspx.cs
+++ Access/AccessApp/LinkObject.aspx.cs
@@ -17,7 +17,29 @@ namespace AccessApp
 
         protected void B_apply_Click(object sender, EventArgs e)
         {
+            if (TB_EpiID.Text != string.Empty && TB_id_op.Text != string.Empty && TB_id_ticket.Text != string.Empty)
+            {
+                DataSet _Ds = DAL.GetProduct(TB_EpiID.Text);
+                if (_Ds.Tables[0].Rows[0]["STOCK_STATUS"].ToString() == "STOCKED" || _Ds.Tables[0].Rows[0]["STOCK_STATUS"].ToString() == "INSTALLED" || _Ds.Tables[0].Rows[0]["STOCK_STATUS"].ToString() == "UNDER_REPAIR")
+                {
+                    if (true)
+                    {
 
+                    }
+                    else
+                    {
+                        System.Web.HttpContext.Current.Response.Write("<SCRIPT LANGUAGE='JavaScript'>alert('Ce matériel a déjà été ajouté')</SCRIPT>");
+                    }
+                }
+                else
+                {
+                    System.Web.HttpContext.Current.Response.Write("<SCRIPT LANGUAGE='JavaScript'>alert('Ce matériel n'est pas dans le stock.')</SCRIPT>");
+                }
+            }
+            else
+            {
+                System.Web.HttpContext.Current.Response.Write("<SCRIPT LANGUAGE='JavaScript'>alert('Champs vides.')</SCRIPT>");
+            }
         }
 
         protected void TB_id_ticket_TextChanged(object sender, EventArgs e)
