13e20dc06d64dd0dca973533d5a4a0cdfc6b4261
Lepape Yorick <yorick-1996@hotmail.com>
2018-03-28 11:46:31 +0200

    add link

---
+++ Access/AccessApp/LinkObject.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Title="LinkObjet" MasterPageFile="~/Site.Master" Language="C#" AutoEventWireup="true" CodeBehind="LinkObject.aspx.cs" Inherits="AccessApp.LinkObject" EnableEventValidation="false" MaintainScrollPositionOnPostback="true" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="page-header" style="text-align: center;">
+        <h2>Lier un matériel</h2>
+    </div>
+    <table>
+        <tr>
+            <td>
+                <p>
+                    <b><asp:Label ID="L_id_ticket" runat="server">Id du ticket</asp:Label></b>
+                    <asp:TextBox ID="TB_id_ticket" CssClass="form-control input-lg" runat="server" name="TB_id_ticket"></asp:TextBox>
+                </p>
+                <p>
+                    <b><asp:Label ID="L_id_op" runat="server">Id de l'opérateur</asp:Label></b>
+                    <asp:TextBox ID="TB_id_op" CssClass="form-control input-lg" runat="server" name="TB_id_op"></asp:TextBox>
+                </p>
+                <p>
+                    <b><asp:Label ID="L_EpiID" runat="server">EpiID</asp:Label></b>
+                    <asp:TextBox ID="TB_EpiID" CssClass="form-control input-lg" runat="server" name="TB_EpiID"></asp:TextBox>
+                </p>
+            </td>
+        </tr>
+        <tr>
+            <td align='right'>
+                <p>
+                    <asp:Button ID="B_apply" CssClass="btn btn-primary btn-lg" runat="server" OnClick="B_apply_Click" Text="Confirmer" Enabled="true"/>
+                </p>
+            </td>
+        </tr>
+    </table>
+
+</asp:Content>
---
+++ Access/AccessApp/LinkObject.aspx.designer.cs
@@ -0,0 +1,78 @@
+﻿//------------------------------------------------------------------------------
+// <généré automatiquement>
+//     Ce code a été généré par un outil.
+//
+//     Les modifications apportées à ce fichier peuvent provoquer un comportement incorrect et seront perdues si
+//     le code est régénéré.
+// </généré automatiquement>
+//------------------------------------------------------------------------------
+
+namespace AccessApp {
+    
+    
+    public partial class LinkObject {
+        
+        /// <summary>
+        /// Contrôle L_id_ticket.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label L_id_ticket;
+        
+        /// <summary>
+        /// Contrôle TB_id_ticket.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TB_id_ticket;
+        
+        /// <summary>
+        /// Contrôle L_id_op.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label L_id_op;
+        
+        /// <summary>
+        /// Contrôle TB_id_op.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TB_id_op;
+        
+        /// <summary>
+        /// Contrôle L_EpiID.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label L_EpiID;
+        
+        /// <summary>
+        /// Contrôle TB_EpiID.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TB_EpiID;
+        
+        /// <summary>
+        /// Contrôle B_apply.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button B_apply;
+    }
+}
